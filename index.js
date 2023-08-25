9287b26fe140bc5a465ca6a6c7180a3652284d73
Joel Rodrigues <joelp.rodrigues25@gmail.com>
2023-08-25 11:05:06 -0300

    add JS to project and create events

---
+++ index.js
@@ -0,0 +1,17 @@
+function onClickHabilidades(){
+  document.getElementById("section-abilities").scrollIntoView();
+}
+
+
+function onClickPortfolio(){
+  document.getElementById("section-portfolio").scrollIntoView();
+}
+
+function onClickExperiencia(){
+  document.getElementById("section-experiencia").scrollIntoView();
+}
+
+function onClickContact(){
+  document.getElementById("section-contact").scrollIntoView();
+}
+
